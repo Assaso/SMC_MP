e004d641c1c14ab294701ff51ab6c569b9c4a91f
Assaso <Gallardo.juanim@gmail.com>
2016-01-07 15:01:45 -0600

    login screen, added.

--- app/src/main/java/com/example/jgallardo/smc_mp/login.java
+++ app/src/main/java/com/example/jgallardo/smc_mp/login.java
@@ -2,12 +2,45 @@ package com.example.jgallardo.smc_mp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity{
+
+    Button login, cancel;
+    EditText user, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        login = (Button)findViewById(R.id.login_button);
+        cancel = (Button)findViewById(R.id.cancel_button);
+
+        user = (EditText)findViewById(R.id.get_user);
+        pass = (EditText)findViewById(R.id.get_password);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user.getText().toString().equals("qc003") && pass.getText().toString().equals("qc16")){
+                    Toast.makeText(getApplicationContext(), "Se ha iniciado sesión", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Datos equibocados, favor de verificar", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
