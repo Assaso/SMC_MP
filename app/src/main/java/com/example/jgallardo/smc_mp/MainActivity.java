323bcef6abac7b07bd98198971e0a9df27e69302
Assaso <Gallardo.juanim@gmail.com>
2016-01-07 13:00:10 -0600

    login screen, added.

--- app/src/main/java/com/example/jgallardo/smc_mp/MainActivity.java
+++ app/src/main/java/com/example/jgallardo/smc_mp/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,16 +18,10 @@ public class MainActivity extends AppCompatActivity {
         logo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               /* Intent intent = new Intent(MainActivity.this, login.class);
-                startActivity(intent); */
-                Toast_g();
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
             }
         });
     }
 
-    public void Toast_g (){
-        String msg = "OK";
-        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
-        toast.show();
-    }
 }
