5d2b192623086c950dfadf45a0567f7c2b4cb5d8
Assaso <Gallardo.juanim@gmail.com>
2016-01-07 11:59:33 -0600

    toast

--- app/src/main/java/com/example/jgallardo/smc_mp/MainActivity.java
+++ app/src/main/java/com/example/jgallardo/smc_mp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.jgallardo.smc_mp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView logo = (ImageView)findViewById(R.id.initial_logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               /* Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent); */
+                Toast_g();
+            }
+        });
+    }
+
+    public void Toast_g (){
+        String msg = "OK";
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
