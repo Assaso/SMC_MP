95cd37b1de57930c20a995ae3aa81a7294707690
Assaso <Gallardo.juanim@gmail.com>
2016-01-11 18:42:00 -0600

    Text custom

--- app/src/main/java/com/example/jgallardo/smc_mp/registrys/registry_general.java
+++ app/src/main/java/com/example/jgallardo/smc_mp/registrys/registry_general.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.jgallardo.smc_mp.R;
 import com.example.jgallardo.smc_mp.inspections.wleding_selection;
@@ -23,12 +25,23 @@ public class registry_general extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registry_general);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
-
         String elegido = extras.getString(wleding_selection.ELEGIDO, "");
+
+        final boolean title = requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+
+        setContentView(R.layout.registry_general);
+
+        if(title){
+            getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
+        }
+
+        final TextView titletext = (TextView)findViewById(R.id.titletext);
+        if(titletext != null){
+            titletext.setText(elegido);
+        }
 
         ok = (Button)findViewById(R.id.registry_ok);
         nc = (Button)findViewById(R.id.registry_nc);
